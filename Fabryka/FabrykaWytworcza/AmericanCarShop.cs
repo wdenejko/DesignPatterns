6a9e252ba1297cda373a40820facd52df96c3d39
Wojtek Denejko <deneyw@gmail.com>
2016-10-24 19:18:31 +0200

    lab02

---
+++ Fabryka/FabrykaWytworcza/AmericanCarShop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace FabrykaWytworcza
+{
+	public class AmericanCarShop : CarShop
+	{
+		public override Car CreateCar(string item)
+		{
+			if (item.Equals("audi"))
+			{
+				return new AmericanAudi();
+			}
+			else if (item.Equals("vw"))
+			{
+				return new AmericanVw();
+			}
+			else return null;
+		}
+	}
+}
---
+++ Fabryka/FabrykaWytworcza/EuropeanCarShop.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace FabrykaWytworcza
+{
+	public class EuropeanCarShop : CarShop
+	{
+		public override Car CreateCar(string item)
+		{
+			if (item.Equals("audi"))
+			{
+				return new EuropeanAudi();
+			}
+			else if (item.Equals("vw"))
+			{
+				return new EuropeanVw();
+			}
+			else return null;
+		}
+	}
+}
